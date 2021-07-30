3c2a3389c7ebf7c381668774a7ad9625e19b93b6
Donald Seder <14dseder@gmail.com>
2021-07-30 15:33:06 -0400

    Get-ComputerTemperature
    Gets reported computer temperature through WMI

---
+++ Get-ComputerTemperature.psm1
@@ -0,0 +1,16 @@
+﻿function Get-ComputerTemperature ($ComputerName){
+    Write-Host "Testing if terminal can be pinged..." -ForegroundColor Yellow
+    if(Test-Connection -ComputerName $ComputerName){
+        Write-Host "$ComputerName is up" -ForegroundColor Green
+    }
+
+    $temperatures = ""
+    $Fahrenheit = ""
+    $temperatures = Get-WmiObject -ComputerName $ComputerName MSAcpi_ThermalZoneTemperature -Namespace "root/wmi" | Select-Object -Property InstanceName,CurrentTemperature
+    foreach ($temp in $temperatures){
+        $Fahrenheit = [math]::round((9/5) * ($temp.CurrentTemperature / 10 - 273.15) + 32)
+        $temp.InstanceName
+        Write-host "Current Temperature: $Fahrenheit F"
+        Write-Host "----------------------------------"
+    }
+}
