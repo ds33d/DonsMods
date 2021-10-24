8be4dfc9b2bc02ca55ca6d064f0c2f11ededf30c
Donald Seder <14dseder@gmail.com>
2021-10-24 10:27:53 -0400

    Add files via upload

---
+++ Get-EmailAddresses.psm1
@@ -0,0 +1,12 @@
+﻿
+function Get-EmailAddress($UserName){
+    Write-Host "Email Addresses:"
+    Write-Host "-----------------------"
+    $userAccount = Get-ADUser -LDAPFilter "(SamAccountName=$UserName)" -Properties proxyAddresses
+    foreach($account in $userAccount.proxyAddresses){
+        $account = $account -replace "smtp:"
+        if ($account -like "*500*"){$null}
+        else{Write-Host $account}
+        
+    }
+}
